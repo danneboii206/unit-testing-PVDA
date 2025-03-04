82d4104b359c81d345da4dc8986988eaf9981961
Daniel Abu Ramadan <daab24@student.bth.se>
2025-03-04 17:08:09 +0100

    add .gitignore & few example unittests

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 import pytest
 
+"""
+def potens(x):
+    return (x * x)
+
+def test_wrong():
+    assert potens(2) == 5
+
+def test_correct():
+    assert potens(2) == 4
+
+def test_wrong2():
+    assert potens(2) == 6
+"""
