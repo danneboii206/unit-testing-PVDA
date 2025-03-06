53426ea0f2a7424d6a4269c51c21c1e09ed96978
Aldin Jusufagic <aldin_jusufagic@hotmail.com>
2025-03-06 16:43:35 +0100

    Revert "removed useless typeError exception"
    This reverts commit 48ef62dadd88bc52c0b08ab5248426dbdca0bfdb.

--- interactWithObj.py
+++ interactWithObj.py
@@ -12,10 +12,13 @@ class Game:
         raise NameError("No such object")
 
     def selectInteraction(self, theInteraction):
-        if theInteraction in self._curretGameObj.listInteractionTypes():
-            self._curretGameObj.setCurrentInteraction(theInteraction)
-            return self._curretGameObj.listCurrentInteractionOptions()
-        return "Not an option!"
+        try:
+            if theInteraction in self._curretGameObj.listInteractionTypes():
+                self._curretGameObj.setCurrentInteraction(theInteraction)
+                return self._curretGameObj.listCurrentInteractionOptions()
+            return "Not an option!"
+        except TypeError: #Fixes when someone uses integers instead of strings (shouldn't be possible anyways as input returns string)
+            return "Not an option!"
 
 class Scene:
     def __init__(self):
